9a7dfbe2d8636a5289f18238b462b5585220032e
giacom0c <42006598+giacom0c@users.noreply.github.com>
2021-12-17 18:21:26 +0100

    Giorno 13 parte 1

---
+++ giorno13/soluzione.py
@@ -0,0 +1,57 @@
+def get_data(filename):
+    coordinate = []
+    mosse = []
+    x_max = y_max = 0
+    with open(filename, 'r') as file:
+        for line in file:
+            if ',' in line:
+                x, y = map(int, line.strip().split(','))
+                coordinate.append((x, y))
+                if x > x_max:
+                    x_max = x
+                if y > y_max:
+                    y_max = y
+            elif 'x=' in line:
+                temp = line.strip().split('=')
+                mosse.append(('x', int(temp[1])))
+            elif 'y=' in line:
+                temp = line.strip().split('=')
+                mosse.append(('y', int(temp[1])))
+            else:
+                continue
+    return coordinate, x_max, y_max, mosse
+
+
+def parte1(coordinate, x_max, y_max, mosse):
+    foglio = []
+    for j in range(y_max + 1):
+        temp = []
+        for i in range(x_max + 1):
+            if (i, j) in coordinate:
+                temp.append('#')
+            else:
+                temp.append('.')
+        foglio.append(temp)
+    mossa = mosse[0]
+    fold = mossa[1]
+    for j in range(1, fold + 1):
+        for i in range(x_max + 1):
+            if foglio[-j][i] == '#':
+                foglio[j - 1][i] = '#'
+        print(foglio[j - 1])
+    conta = 0
+    for j in range(fold + 1):
+        for i in range(x_max + 1):
+            if foglio[j][i] == '#':
+                conta += 1
+    return conta
+
+
+def parte2(filename):
+    print('TODO')
+
+
+filename = 'input.txt'
+coordinate, x_max, y_max, mosse = get_data(filename)
+print(parte1(coordinate, x_max, y_max, mosse))
+parte2(filename)
