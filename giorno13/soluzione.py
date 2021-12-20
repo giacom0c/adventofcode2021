97bd58de6402e11d5a01c42cdb59528c2845650a
G0D0T <giacomocalabresi96@gmail.com>
2021-12-20 18:36:22 +0100

    Giorno 13 parte 2

--- giorno13/soluzione.py
+++ giorno13/soluzione.py
@@ -22,7 +22,7 @@ def get_data(filename):
     return coordinate, x_max, y_max, mosse
 
 
-def parte1(coordinate, x_max, y_max, mosse):
+def piega(coordinate, x_max, y_max, mosse):
     foglio = []
     for j in range(y_max + 1):
         temp = []
@@ -40,27 +40,34 @@ def parte1(coordinate, x_max, y_max, mosse):
                 for i in range(x_max + 1):
                     if foglio[-j][i] == '#':
                         foglio[j - 1][i] = '#'
-                print(foglio[j - 1])
+                #print(foglio[j - 1])
         else:
             x_max = m[1]
             for j in range(y_max + 1):
                 for i in range(1, x_max + 1):
                     if foglio[j][-i] == '#':
                         foglio[j][i - 1] = '#'
-                print(foglio[j][:x_max])
+                #print(foglio[j][:x_max])
     conta = 0
-    for j in range(fold + 1):
+    for j in range(y_max + 1):
         for i in range(x_max + 1):
             if foglio[j][i] == '#':
                 conta += 1
     return conta
 
 
-def parte2(filename):
-    print('TODO')
+def parte1(coordinate, x_max, y_max, mosse):
+    totale = piega(coordinate, x_max, y_max, mosse)
+    print(totale)
+
+
+def parte2(coordinate, x_max, y_max, mosse):
+    totale = piega(coordinate, x_max, y_max, mosse)
+    print(totale)
 
 
 filename = 'input.txt'
 coordinate, x_max, y_max, mosse = get_data(filename)
-print(parte1(coordinate, x_max, y_max, mosse))
-parte2(filename)
+m = [mosse[0]]
+parte1(coordinate, x_max, y_max, m)
+#parte2(coordinate, x_max, y_max, mosse)
